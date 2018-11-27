168d1b8d3d6ae3a3f690b29f7673e5af4f416235
Strahl Balazs <balazs.strahl@gmail.com>
2018-11-27 16:29:31 +0100

    VGA axi még nincs tesztelve

--- ddr3_dma/ddr3_dma.srcs/sources_1/imports/vga_jatekkal_teszt/VGA_Control.v
+++ ddr3_dma/ddr3_dma.srcs/sources_1/imports/vga_jatekkal_teszt/VGA_Control.v
@@ -3,19 +3,18 @@
 module vgaSync (
    input wire clk,
 
-   input wire [2:0] rin,
-   input wire [2:0] gin,
-   input wire [1:0] bin,
+   input wire [7:0] wdata,
+   input wire wvalid,
+   input wire wlast,
+   output reg wready,
+    
 
    output reg [2:0] rout,
    output reg [2:0] gout,
    output reg [1:0] bout,
 
    output reg hs,
-   output reg vs,
-
-   output wire [10:0] hc,
-   output wire [10:0] vc
+   output reg vs
    );
 
    // VGA 640x480@60Hz,25MHz
@@ -82,6 +81,59 @@ module vgaSync (
          vs = ~vsyncpolarity;
     end
 
+
+reg [1:0] state=0;
+reg [7:0] data;
+
+parameter wait_for_wlast=0;
+parameter wait_for_origo=1;
+parameter vga_write_pixels=2;
+
+   always @*
+   begin
+    if(state == wait_for_wlast)
+    begin
+        wready <= 1'b1;                 // tudunk adatot fogadni
+        if(wlast)
+        begin
+            wready <= 1'b0;             // nem tudunk adatot fogadni
+            state <= wait_for_origo;
+        end
+        // Különben várunk
+    end
+    else if(state == wait_for_origo)    
+    begin
+        if(hcount==0 && vcount==0)      // a VGA vezérlõ a (0,0)-s pixelnél tart
+        begin
+            state <= vga_write_pixels;
+            wready <= 1'b1;                 // tudunk adatot fogadni
+        end
+        // Különben várunk
+    end
+    else if(state == vga_write_pixels)      // TÉNYLEGES KÉPERNYÕREÍRÁS 
+    begin
+        if(wvalid)
+        begin
+            if(active_area)
+            begin
+                 wready <= 1'b1;                 // tudunk adatot fogadni
+                 data <= wdata;
+                 
+                 gout = data[1:0];
+                 rout = data[3:2];
+                 bout = data[5:4];
+            end
+            else
+            begin                           // az aktív részen kívül vagyunk
+                wready <= 1'b0;             // nem tudunk adatot fogadni
+            end
+        end
+        else                                // Szinkronizációs hiba
+            state <= wait_for_wlast;        
+    end
+    
+   end
+/*
    always @*
 	begin
       if (active_area)
@@ -97,6 +149,7 @@ module vgaSync (
          bout = 2'b0;
       end
    end
+   */
 endmodule
 
 
